647e32cb61a2219311e6520348af3b4d853b04de
Samantha Fontanez <samantha.fontanez20@spsmail.cuny.edu>
2024-09-08 11:30:02 -0400

    please work

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,22 @@
 
-def list_divide(numbers, divide):
-    """
-    The function returns the number of elements in the numbers list that are divisibleby divide
-    """
+def list_divide(numbers, divide=2):
+    if divide == 0:
+        raise ListDivideException()
+    return sum(1 for number in numbers if number % divide ==0)
+class ListDivideException(Exception):
+    pass
+   
     pass
 
 def test_list_divide():
-    """
-    Test listDivide
-    """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
+    assert list_divide([1,2,3,4,5]) == 2
+    assert list_divide([2,4,6,8,10]) == 5
+    assert list_divide([30, 54, 63,98, 100], divide=10) == 2
+    assert list_divide([]) == 0
+    assert list_divide([1,2,3,4,5], 1) == 5
+    print("All tests passed!")
+    
+print("A test failed.")
     
 if __name__ == "__main__":
-    testListDivide()
+    test_list_divide()
--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,20 @@
-
 class Book:
-    def __init__(self, author, title):
-        pass
+        def __init__(self, author="", title=""):
+            """
+            Constructor of a Book's Author and Title
+            :param Author: The name of the author
+            :param Title: the name of the book
+            """
+            self.author = author
+            self.title = title
     
-    def display(self):
-        pass
-
+        def display(self):
+            """Display the Book's Author and title"""
+            print(f"{self.title}, written by {self.author}")
 
 if __name__ == "__main__":
-    pass
+    Book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+    Book2 = Book("Walter Scott", "Ivanhoe: A Romance")
+
+Book1.display()
+Book2.display()
